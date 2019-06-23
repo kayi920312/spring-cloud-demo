1a1b407d8cac37736855f1967156183a3b96e74b
kayi920312 <kaiying.chen@qq.com>
2019-06-23 19:33:21 +0800

    eureka server 高可用

--- microservice-movie/src/main/java/com/example/demo/controller/MovieController.java
+++ microservice-movie/src/main/java/com/example/demo/controller/MovieController.java
@@ -1,16 +1,15 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.example.demo.bean.User;
 import com.example.demo.common.JsonResult;
 
 import io.swagger.annotations.Api;
@@ -23,10 +22,13 @@ public class MovieController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 	private static final String USER_SERVICE_URL = "http://localhost:8081";
+	private static final String USER_SERVICE_URL_BALANCE = "http://microservice-user";
 	private static final String GET_USERINFO = "/userList?";
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
 	
 	@ApiOperation(value = "用户信息查询", notes="")
 	@GetMapping(value = "/userList")
@@ -35,11 +37,23 @@ public class MovieController {
 			@ApiParam("姓名") @RequestParam(value="name", required=false) String name) {
 		
 		try {
-			JsonResult result = restTemplate.getForObject(USER_SERVICE_URL + GET_USERINFO + "id=" + id + "&name=" + name, JsonResult.class);
+			JsonResult result = restTemplate.getForObject(USER_SERVICE_URL_BALANCE + GET_USERINFO + "id=" + id + "&name=" + name, JsonResult.class);
 			return result;
 		} catch (Exception e) {
 			LOGGER.error("error: " + e.toString());
 			return new JsonResult(false, "失败: " + e.getMessage());
+		}
+	}
+	
+	@ApiOperation(value = "测试Ribbon", notes="")
+	@GetMapping(value = "/log-user-instance")
+	public void getLogUserInstance() {
+		
+		try {
+			ServiceInstance userInstance = this.loadBalancerClient.choose("microservice-user");
+			LOGGER.info(userInstance.getHost() + ": " + userInstance.getPort());
+		} catch (Exception e) {
+			LOGGER.error("error: " + e.toString());
 		}
 	}
 }
